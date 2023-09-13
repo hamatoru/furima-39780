13394caec0c5fb0f5f682192f0eb68bb4e395bb4
hamatoru <hamatoto3@gmail.com>
2023-09-13 19:06:10 +0900

    ユーザー管理機能　登録系確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナ' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナ' }
+    validates :birthday
+
+  with_options uniqueness: true do
+    validates :email
+    end
+  end
 end
