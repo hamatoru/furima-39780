7411f6532814964396ac9843d507427c0d4a7d85
hamatoru <hamatoto3@gmail.com>
2023-09-17 10:03:00 +0900

    商品詳細表示機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   @items = Item.all.order("created_at DESC")
@@ -19,12 +19,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
 private
 def item_params
   params.require(:item).permit(:image, :item_name, :item_info, :category_id, :sale_status_id, :shipping_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
 end
-
-
 
 
 end
