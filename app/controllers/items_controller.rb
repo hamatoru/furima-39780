ee7c8242f6c0f22a19e2dcdf16301832c6690fb2
hamatoru <hamatoto3@gmail.com>
2023-09-17 21:39:46 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit]
 
   def index
   @items = Item.all.order("created_at DESC")
@@ -23,11 +23,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+
+    if @item.user_id != current_user.id
+    redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
 private
 def item_params
   params.require(:item).permit(:image, :item_name, :item_info, :category_id, :sale_status_id, :shipping_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
 end
-
 
 end
